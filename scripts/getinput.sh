2504837218af7079c51faf33ee62d4f443e4458c
Francis Begyn <francis@begyn.be>
2022-11-30 12:57:50 +0100

    update year

--- scripts/getinput.sh
+++ scripts/getinput.sh
@@ -5,12 +5,12 @@ DIR=day$(printf "%02d" ${DAY})
 
 mkdir -p ./inputs/${DIR}/
 
-curl "https://adventofcode.com/2021/day/${DAY}/input" \
+curl "https://adventofcode.com/2022/day/${DAY}/input" \
   -H 'authority: adventofcode.com' \
   -H 'pragma: no-cache' \
   -H 'cache-control: no-cache' \
   -H 'upgrade-insecure-requests: 1' \
-  -H "referer: https://adventofcode.com/2021/day/${DAY}" \
+  -H "referer: https://adventofcode.com/2022/day/${DAY}" \
   -H "cookie: ${AOCCOOKIE}" \
   --compressed \
   -fSL -o ./inputs/${DIR}/input.txt \
